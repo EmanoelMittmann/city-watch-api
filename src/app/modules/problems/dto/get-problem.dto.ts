52d9ccb5821275268de1bb67bbc1122bc10b2a2a
ArthurLopes191 <arthulopesreal@gmail.com>
2024-10-23 22:40:52 -0300

    feat/convert-enum-problemtype-to-number

--- src/app/modules/problems/dto/get-problem.dto.ts
+++ src/app/modules/problems/dto/get-problem.dto.ts
@@ -1,22 +1,29 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 export class GetProblemDto {
     @ApiProperty()
+    @IsString()
     name: string;
 
     @ApiProperty()
+    @IsString()
     address: string;
 
     @ApiProperty()
+    @IsString()
     description: string;
 
     @ApiProperty()
+    @IsNumber()
     latitude: number;
 
     @ApiProperty()
+    @IsNumber()
     longitude: number;
 
     @ApiProperty()
+    @IsString()
     photo: string;
 
     @ApiProperty({ enum: [1, 2, 3, 4, 5] })
